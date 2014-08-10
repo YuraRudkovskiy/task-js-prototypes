e5779669b8b61da9d72cacbd1e022691223b06c2
Yuriy Rudkovskiy <yur@qapint.com>
2014-08-10 13:54:24 +0300

    Add first objects.WIP

--- scripts/fight.js
+++ scripts/fight.js
@@ -1,5 +1,35 @@
 (function(){
 	"use strict"
+	var Cat = new CatMonster('Tom'),
+		Duck = new BirdMonster('Scrooge McDuck');
 
+	console.log(Cat);
+	console.log(Duck);
+
+	Cat.sayName();
+	Duck.sayName();
+
+	Cat.sayHp();
+	Duck.sayHp();
+
+	Cat.scratch.call(Duck);
+
+	Cat.sayHp();
+	Duck.sayHp();
 	
+
+	Cat.scratch.call(Duck);
+	Duck.peck.call(Cat);
+	Cat.scratch.call(Duck);
+	Duck.peck.call(Cat);
+	Cat.scratch.call(Duck);
+	Cat.scratch.call(Duck);
+	Duck.peck.call(Cat);
+	Duck.peck.call(Cat);
+	Duck.peck.call(Cat);
+	Duck.peck.call(Cat);
+	
+	Cat.sayHp();
+	Duck.sayHp();
+
 }());
--- scripts/monstersfactory.js
+++ scripts/monstersfactory.js
@@ -1,5 +1,56 @@
 (function(){
 	"use strict"
 
-	
+	function Monster(name, currentHealth, initialHealth){
+		this.name = name;
+		this.currentHealth = currentHealth;
+		this.initialHealth = initialHealth;
+	};
+
+	Monster.prototype.sayName = function(){
+		console.log(this.name);
+	};
+
+	Monster.prototype.sayHp = function(){
+		console.log(this.currentHealth);
+	};
+
+	Monster.prototype.changeDamage = function(damage){
+		if(this.currentHealth < damage){
+			this.currentHealth = 0;
+			console.log(this.name, ' died.');
+		}else{
+			this.currentHealth -= damage;
+			console.log(this.name, ' caused damage in ' + damage + ' HP');
+		}
+	};
+
+
+
+	function CatMonster(name){
+		Monster.call(this, name, 50, 50);
+	};
+
+	CatMonster.prototype = Object.create(Monster.prototype);
+
+	CatMonster.prototype.scratch = function(){
+		this.changeDamage(5);
+	}
+
+
+
+	function BirdMonster(name){
+		Monster.call(this, name, 60, 60);
+	};
+
+	BirdMonster.prototype = Object.create(Monster.prototype);
+
+	BirdMonster.prototype.peck = function(){
+		this.changeDamage(3);
+	}
+
+
+	window.CatMonster = CatMonster;
+	window.BirdMonster = BirdMonster;
+
 }());
